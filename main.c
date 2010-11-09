24d3242fc0940146fc7f477ca70dafb0b5ee6357
Jacob Watkins <dividehex@yahoo.com>
2010-11-09 13:37:41 -0800

    Added more functions and structures
    This now contains a menu manager for the user to
    select from.  Also, added initHashBST.

--- main.c
+++ main.c
@@ -10,12 +10,14 @@ main.c - main source
 int main( void )
 {
 	// Local Definitions
-
+	HEADER* listHead = NULL;
 
 	// Statements
 	printf("\n\n\t\t\t15C TEAM PROJECT\n\n");
 	
+	listHead = initHashBST();
 
+	menuManager( listHead );
 
     printf("\n");
 	printf( _CrtDumpMemoryLeaks() ? "Memory Leak\n" : "No Leak\n");
@@ -23,3 +25,126 @@ int main( void )
 	system("PAUSE");
 	return 0;
 }
+
+/*	================= printMenu ===================
+	Prints main menu
+	   Pre    none
+	   Post   Prints menu to stdout
+	   Return void
+*/
+void printMenu( void )
+{
+	// Local Definitions
+	// Statements
+	printf("\n\t\t*************************************");
+    printf("\n\t\t*                                   *");
+	printf("\n\t\t*   1. Add new package              *");
+	printf("\n\t\t*   2. Delete package               *");
+	printf("\n\t\t*   3. Search Package by name       *");
+	printf("\n\t\t*   4. List Packages (unsorted)     *");
+	printf("\n\t\t*   5. List Packages (sorted)       *");
+	printf("\n\t\t*   6. Print Tree                   *");
+	printf("\n\t\t*   7. Write to file                *");
+	printf("\n\t\t*   8. Calculate Efficiency         *");
+	printf("\n\t\t*   9. Quit                         *");
+	printf("\n\t\t*                                   *");
+	printf("\n\t\t*************************************");
+	return;
+} // printMenu
+
+
+/* ============================ menuManager =======================
+	Process user's option by calling appropriate functions.
+	   PRE  : 
+	   POST : 
+*/
+void menuManager( HEADER* listHeader )
+{
+	// Local Definitions
+	char option;
+
+	// Statements
+	printMenu ();
+	do
+	{
+		option = getChar ();
+	    option = toupper (option);
+		switch(option)
+		{
+		    case '1' : ;
+				       break;
+		    case '2' : ;
+				       break;
+		    case '3' : ;
+				       break;
+			case '4' : ;
+				       break;
+		    case '5' : ;
+				       break;
+			case '6' : ;
+                       break;
+			case '7' : ;
+				       break;
+			case '8' : ;
+				       break;
+			case '9' : printf("Closing program\n");
+				       break;
+			case 'M' : printMenu();
+				       break;
+            default  : printf("\n*** Invalid option! ***\n");
+		               printf("Enter one of the following numbers: "
+			                  "1, 2, 3, 4, 5, 6, 7, 8, 9: " );
+					   break;
+		} 
+	} while (option != '9');
+	return;
+}  // menuManager
+
+/* ============================ getChar =======================
+	Gets char from user and returns it
+	   PRE  : 
+	   POST : 
+	   RETURNS : option - char from user input
+*/
+char getChar ( void )
+{
+	// Local Definitions
+	char option;
+
+	// Statements
+	printf ("\n\nPlease enter an option (M - for Menu): ");
+	while (scanf ("%c", &option) == 0)
+	{
+		FLUSH;
+		printf("Invalid Input. Please try again\n");
+		printf("\n\nPlease enter an option (M - for Menu): ");
+	}
+	FLUSH;
+	return option;
+}  // getChar
+
+
+/* ============================ initHashBST =======================
+	Allocates a header and initializes default values
+	   PRE  : 
+	   POST : 
+	   RETURNS : ptr to allocated header struct
+*/
+HEADER* initHashBST ( void )
+{
+	// Local Definitions
+	HEADER* listHeader = NULL;
+	
+	// Statements
+	if(!(listHeader = (HEADER*) malloc( sizeof(HEADER))))
+		MEM_ERR;
+
+	listHeader->count = 0;
+	listHeader->bucketSize = BUCKETSIZE;
+	listHeader->hashArySize = 0;
+
+	listHeader->hashAry = NULL;
+	listHeader->treeRoot = NULL;
+
+	return listHeader;
+}  // initHashBST
--- main.h
+++ main.h
@@ -11,13 +11,10 @@ main.h - main header
 #include <ctype.h>
 #include <crtdbg.h>
 
-
-
- 
-
-
 // #defines
 #define FLUSH while (getchar () != '\n')
+#define MEM_ERR printf( "Not enough memory\n" ), exit(201)
+#define BUCKETSIZE 3
 
 // Data Structures
 typedef struct package {
@@ -27,14 +24,50 @@ typedef struct package {
 	char *description;  // software description
 } PACKAGE;
 
+typedef struct bucket {
+	char* key;
+	PACKAGE* ptrPackage;
+} BUCKET;
+
+typedef struct hash {
+	int buckets_used;
+	BUCKET bucket[BUCKETSIZE];
+} HASH;
+
+typedef struct bstnode {
+	PACKAGE* ptrPackage;
+	struct node* left;
+	struct node* right;
+} BSTNODE;
+
 typedef struct header {
 	int count;
-	int arySize;
-	// pHash
-	// pTree
+	int hashArySize;
 	int bucketSize;
+	HASH *hashAry;
+	BSTNODE *treeRoot;
 } HEADER;
 
-
 // Function Prototypes
+// Driver functions
+void printMenu( void );
+void menuManager( HEADER* listHeader );
+char getChar ( void );
+void loadData ( HEADER* listHeader );
+void addPackage ( HEADER* listHeader );
+void deletePackage ( HEADER* listHeader );
+HEADER* initHashBST ( void );
 
+// Hash Functions
+void insertHash ( void );
+void hashSearch ( void );
+void listHash ( void );
+void hashEff ( void );
+
+//BST Functions
+void insertTree ( void );
+void listTree ( void );
+void printTree ( void );
+
+// File I/O Functions
+void writeFile ( void );
