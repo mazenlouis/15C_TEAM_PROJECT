46f10058385993d9a3d915cce43ddbf09434be9f
unknown <dunhill@.business-cs.deanza.ad.fhda.edu>
2010-11-09 17:39:02 -0800

    Start of the file_io read file functions

--- file_io.c
+++ file_io.c
@@ -2,3 +2,32 @@
 
 // #includes
 #include "main.h"
+
+
+
+#define FILENAME "input.txt"
+
+/*  ===================== getData ======================
+	This function reads the file and gathers the data into nodes
+	to be placed into the BST and Hash Table.
+		Pre: list - empty list
+			 listName - empty list
+		Post: list - BST of nodes about students by pin
+		      listName - BST of nodes about student by name
+*/
+void loadData ( HEADER* listHeader )
+{
+		PACKAGE* tempPack;
+		FILE* fpIn;
+		char* packageNameTemp;
+		char* versionTemp;
+		char* descriptionTemp;
+		int levelTemp;
+
+		fpIn = fopen(FILENAME, "r");
+
+
+		tempPack = createPackage();
+
+		return;
+} // end getData
