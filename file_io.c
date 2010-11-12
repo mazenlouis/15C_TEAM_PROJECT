04b87f48fa999b5939f7b6aa618d23b2b1e95735
Jacob Watkins <dividehex@yahoo.com>
2010-11-12 12:31:40 +0800

    Added allocatePackage and parseLine funcs

--- file_io.c
+++ file_io.c
@@ -3,10 +3,6 @@
 // #includes
 #include "main.h"
 
-
-
-#define FILENAME "input.txt"
-
 /*  ===================== loadData ======================
 	This function reads the file and gathers the data into nodes
 	to be placed into the BST and Hash Table.
@@ -15,46 +11,68 @@
 */
 void loadData ( HEADER* listHeader )
 {
-		PACKAGE* tempPackage;
-		FILE* fpIn;
-		char packageNameTemp[99];
-		char versionTemp[99];
-		char descriptionTemp[99];
-		int levelTemp;
+	PACKAGE* Package;
+	FILE* fpIn;
+	char line[300];
+	char packageNameTemp[99];
+	char versionTemp[99];
+	char descriptionTemp[99];
+	int levelTemp = 0;
 
-		fpIn = fopen(FILENAME, "r");
-		if(!fpIn)
-			MEM_ERR;
+	fpIn = fopen(INPUTFILE, "r");
+	if(!fpIn)
+		MEM_ERR;
 
-		while() // loop that will read file line-by-line saving data into the above temp variables
+	while(fgets( line, sizeof(line), fpIn )) // loop that will read file line-by-line saving data into the above temp variables
+	{
+		if( parseLine( line, packageNameTemp, versionTemp, levelTemp, descriptionTemp ))
 		{
-			tempPackage = (PACKAGE*) malloc (sizeof(PACKAGE));
-			if(!tempPackage)
-				MEM_ERR;
-
-			tempPackage->description = (char*) malloc (sizeof (strlen (descriptionTemp) + 1 ));
-			if(!tempPackage->description)
-				MEM_ERR;
-
-			tempPackage->packageName = (char*) malloc (sizeof (strlen (packageNameTemp) + 1 ));
-			if(!tempPackage->packageName)
-				MEM_ERR;
-
-			tempPackage->version = (char*) malloc (sizeof (strlen (versionTemp) + 1 ));
-			if(!tempPackage->version)
-				MEM_ERR;
-
-			strcpy(tempPackage->description, descriptionTemp);
-			strcpy(tempPackage->packageName, packageNameTemp);
-			strcpy(tempPackage->version, versionTemp);
-			tempPackage->level = levelTemp;
-
-			listHeader->count++;
-
-			insertTree(tempPackage);
-			insertHash(tempPackage);
-
+			//Package = allocatePackage(packageNameTemp, versionTemp, levelTemp, descriptionTemp);
+			printf("%s\n", line);
+			//insertTree(Package);
+			//insertHash(Package);
 		}
-
-		return;
+			
+	}
+	fclose(fpIn);
+	return;
 } // end loadData
+
+PACKAGE *allocatePackage( char *packageNameTemp, char *versionTemp, int levelTemp, char *descriptionTemp)
+{
+	// local definitions
+	PACKAGE *allocatedPackage = NULL;
+
+	// statements
+	allocatedPackage = (PACKAGE*) malloc (sizeof(PACKAGE));
+	if(!allocatedPackage)
+		MEM_ERR;
+
+	allocatedPackage->packageName = (char*) calloc (strlen (packageNameTemp) + 1 , sizeof (char));
+	if(!allocatedPackage->packageName)
+		MEM_ERR;
+
+	allocatedPackage->version = (char*) calloc (strlen (versionTemp) + 1 , sizeof (char));
+	if(!allocatedPackage->version)
+		MEM_ERR;
+
+	allocatedPackage->description = (char*) calloc (strlen (descriptionTemp) + 1 , sizeof (char));
+	if(!allocatedPackage->description)
+		MEM_ERR;
+
+	strcpy(allocatedPackage->packageName, packageNameTemp);
+	strcpy(allocatedPackage->version, versionTemp);
+	allocatedPackage->level = levelTemp;
+	strcpy(allocatedPackage->description, descriptionTemp);
+	
+	return allocatedPackage;
+}
+
+int parseLine (char *line, char *packageNameTemp, char *versionTemp, int levelTemp, char *descriptionTemp)
+{
+	int success = 1;
+	char tempLevel[99];
+	sscanf( line, " %s %s %s %s", packageNameTemp, versionTemp, &tempLevel, descriptionTemp);
+	printf("%s s% s% s%\n", packageNameTemp, versionTemp, tempLevel, descriptionTemp);
+	return success;
+}
